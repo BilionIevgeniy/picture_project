6a70543071c9f70d5139ca7fee737fae30e7f68d
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-03-18 11:54:13 +0100

    refactored modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,115 +1,7 @@
+import { calcScroll } from "./helpers";
+
+let btnPressed = false;
 const modals = () => {
-  let btnPressed = false;
-
-  function bindModal(
-    triggerSelector,
-    modalSelector,
-    closeSelector,
-    destroy = false,
-  ) {
-    const trigger = document.querySelectorAll(triggerSelector),
-      modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll("[data-modal]"),
-      scroll = calcScroll();
-
-    trigger.forEach((item) => {
-      item.addEventListener("click", (e) => {
-        if (e.target) {
-          e.preventDefault();
-        }
-
-        btnPressed = true;
-
-        if (destroy) {
-          item.remove();
-        }
-
-        windows.forEach((item) => {
-          item.style.display = "none";
-          item.classList.add("animated", "fadeIn");
-        });
-
-        modal.style.display = "block";
-        document.body.style.overflow = "hidden";
-        document.body.style.marginRight = `${scroll}px`;
-      });
-    });
-
-    close.addEventListener("click", () => {
-      windows.forEach((item) => {
-        item.style.display = "none";
-      });
-
-      modal.style.display = "none";
-      document.body.style.overflow = "";
-      document.body.style.marginRight = `0px`;
-    });
-
-    modal.addEventListener("click", (e) => {
-      if (e.target === modal) {
-        windows.forEach((item) => {
-          item.style.display = "none";
-        });
-
-        modal.style.display = "none";
-        document.body.style.overflow = "";
-        document.body.style.marginRight = `0px`;
-      }
-    });
-  }
-
-  function showModalByTime(selector, time) {
-    setTimeout(function () {
-      let display;
-
-      document.querySelectorAll("[data-modal]").forEach((item) => {
-        if (getComputedStyle(item).display !== "none") {
-          display = "block";
-        }
-      });
-
-      if (!display) {
-        document.querySelector(selector).style.display = "block";
-        document.body.style.overflow = "hidden";
-        let scroll = calcScroll();
-        document.body.style.marginRight = `${scroll}px`;
-      }
-    }, time);
-  }
-
-  function calcScroll() {
-    let div = document.createElement("div");
-
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.overflowY = "scroll";
-    div.style.visibility = "hidden";
-
-    document.body.appendChild(div);
-    let scrollWidth = div.offsetWidth - div.clientWidth;
-    div.remove();
-
-    return scrollWidth;
-  }
-
-  function openByScroll(selector) {
-    window.addEventListener("scroll", () => {
-      let scrollHeight = Math.max(
-        document.documentElement.scrollHeight,
-        document.body.scrollHeight,
-      );
-
-      if (
-        !btnPressed &&
-        window.pageYOffset + document.documentElement.clientHeight >=
-          scrollHeight
-      ) {
-        document.querySelector(selector).click();
-      }
-    });
-  }
-
   bindModal(".button-design", ".popup-design", ".popup-design .popup-close");
   bindModal(
     ".button-consultation",
@@ -120,5 +12,95 @@ const modals = () => {
   openByScroll(".fixed-gift");
   // showModalByTime('.popup-consultation', 5000);
 };
+
+function bindModal(
+  triggerSelector,
+  modalSelector,
+  closeSelector,
+  destroy = false,
+) {
+  const trigger = document.querySelectorAll(triggerSelector),
+    modal = document.querySelector(modalSelector),
+    close = document.querySelector(closeSelector),
+    windows = document.querySelectorAll("[data-modal]");
+
+  trigger.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      if (e.target) {
+        e.preventDefault();
+      }
+      btnPressed = true;
+      if (destroy) {
+        item.remove();
+      }
+      removeWindows(windows, true);
+      setModal(modal);
+    });
+  });
+
+  close.addEventListener("click", () => {
+    removeWindows(windows);
+    resetModal(modal);
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      removeWindows(windows);
+      resetModal(modal);
+    }
+  });
+}
+
+function removeWindows(windows, addClass) {
+  windows.forEach((item) => {
+    item.style.display = "none";
+    addClass && item.classList.add("animated", "fadeIn");
+  });
+}
+
+function setModal(modal) {
+  const scroll = calcScroll();
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+  document.body.style.marginRight = `${scroll}px`;
+}
+
+function resetModal(modal) {
+  modal.style.display = "none";
+  document.body.style.overflow = "";
+  document.body.style.marginRight = `0px`;
+}
+
+function showModalByTime(selector, time) {
+  setTimeout(function () {
+    let display;
+
+    document.querySelectorAll("[data-modal]").forEach((item) => {
+      if (getComputedStyle(item).display !== "none") {
+        display = "block";
+      }
+    });
+
+    if (!display) {
+      setModal(document.querySelector(selector));
+    }
+  }, time);
+}
+
+function openByScroll(selector) {
+  window.addEventListener("scroll", () => {
+    let scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight,
+    );
+
+    if (
+      !btnPressed &&
+      window.pageYOffset + document.documentElement.clientHeight >= scrollHeight
+    ) {
+      document.querySelector(selector).click();
+    }
+  });
+}
 
 export default modals;
